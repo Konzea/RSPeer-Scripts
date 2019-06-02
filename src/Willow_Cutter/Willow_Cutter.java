eba54f0f8ff2eae15c8e338c00ca0e73a4d7c271
James - Desktop <willcocksj@hotmail.co.uk>
2019-06-02 21:18:36 +0100

    Initial Commit: file structure and basic woodcutting script structure

---
+++ src/Willow_Cutter/Willow_Cutter.java
@@ -0,0 +1,18 @@
+package Willow_Cutter;
+
+import org.rspeer.script.Script;
+import org.rspeer.script.ScriptCategory;
+import org.rspeer.script.ScriptMeta;
+
+@ScriptMeta(desc = "Cuts Willow trees, duh.", developer = "Shteve", name = "Willow Cutter", category = ScriptCategory.WOODCUTTING, version = 1)
+public class Willow_Cutter extends Script {
+
+
+
+
+    @Override
+    public int loop() {
+
+        return 3050;
+    }
+}
