784f666122f4cb43841ddfeec1db81ccd7689c8d
James - Desktop <willcocksj@hotmail.co.uk>
2019-06-07 03:30:00 +0100

    Added gear equip methods

--- src/Combat_Starter/Executes/Fighting.java
+++ src/Combat_Starter/Executes/Fighting.java
@@ -1,9 +1,15 @@
 package Combat_Starter.Executes;
 
-import Combat_Starter.Enums.Target;
-import org.rspeer.runetek.api.component.tab.Combat;
-import org.rspeer.runetek.api.component.tab.Skill;
-import org.rspeer.runetek.api.component.tab.Skills;
+import Combat_Starter.Enums.ScriptState;
+import Combat_Starter.Main;
+import org.rspeer.runetek.adapter.component.Item;
+import org.rspeer.runetek.adapter.scene.Npc;
+import org.rspeer.runetek.api.commons.Time;
+import org.rspeer.runetek.api.component.tab.*;
+import org.rspeer.runetek.api.scene.Players;
+import org.rspeer.ui.Log;
+
+import java.util.function.Predicate;
 
 public class Fighting {
 
@@ -18,11 +24,44 @@ public class Fighting {
 //attack
 //else go to fight zone
 //else equip gear
+
+
+            if (Main.getCurrentTarget().getZone().contains(Players.getLocal().getPosition())) {
+                Main.updateScriptState(null);
+            }else
+                Main.updateScriptState(ScriptState.WALKING);
+
+    }
+
+    private static void fightNPC(Predicate<Npc> targetPredicate){
+
+    }
+
+    public static boolean gearEquipped(){
+        return Equipment.isOccupied(EquipmentSlot.MAINHAND) && Equipment.isOccupied(EquipmentSlot.OFFHAND);
+    }
+
+    public static void equipGear(){
+        int attempt = 0;
+        while (!gearEquipped() && attempt < 3) {
+            Item[] items = Inventory.getItems(x -> x.containsAction("Wield") || x.containsAction("Wear"));
+            for (Item i : items) {
+                i.click();
+                Time.sleep(50, 300);
+            }
+            Time.sleepUntil(Inventory::isEmpty, 2000);
+            attempt++;
+        }
+        if (!gearEquipped()){
+            Log.info("Attempted to equip gear 3 times but failed.");
+            Main.updateScriptState(null);
+        }
     }
 
     /**
-     * Returns the lowest 
-     * */
+     * Calculates the lowest combat skill and returns the attack style that will train it.
+     * @return Returns the most optimal attack style to train.
+     */
     public static Combat.AttackStyle getBestAttackStyle(){
         int attackLevel = Skills.getLevel(Skill.ATTACK);
         int strengthLevel = Skills.getLevel(Skill.STRENGTH);
