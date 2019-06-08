34e28cce0709aec7f9d0e59c0e107d80528bb121
James - Desktop <willcocksj@hotmail.co.uk>
2019-06-08 17:18:56 +0100

    Initial combat logic commit & moved gear equipping to own class

--- src/Combat_Starter/Executes/Fighting.java
+++ src/Combat_Starter/Executes/Fighting.java
@@ -1,11 +1,15 @@
 package Combat_Starter.Executes;
 
 import Combat_Starter.Enums.ScriptState;
+import Combat_Starter.Enums.Target;
 import Combat_Starter.Main;
 import org.rspeer.runetek.adapter.component.Item;
 import org.rspeer.runetek.adapter.scene.Npc;
+import org.rspeer.runetek.adapter.scene.Player;
 import org.rspeer.runetek.api.commons.Time;
 import org.rspeer.runetek.api.component.tab.*;
+import org.rspeer.runetek.api.movement.Movement;
+import org.rspeer.runetek.api.scene.Npcs;
 import org.rspeer.runetek.api.scene.Players;
 import org.rspeer.ui.Log;
 
@@ -13,70 +17,58 @@ import java.util.function.Predicate;
 
 public class Fighting {
 
-    private Fighting(){
+    private Fighting() {
         //Private Default Constructor
     }
 
-    public static void execute(){
-//Fighting
-//if gear equipped
-//if in fight zone
-//attack
-//else go to fight zone
-//else equip gear
+    public static void execute() {
 
-
-            if (Main.getCurrentTarget().getZone().contains(Players.getLocal().getPosition())) {
-                Main.updateScriptState(null);
-            }else
-                Main.updateScriptState(ScriptState.WALKING);
-
-    }
-
-    private static void fightNPC(Predicate<Npc> targetPredicate){
-
-    }
-
-    public static boolean gearEquipped(){
-        return Equipment.isOccupied(EquipmentSlot.MAINHAND) && Equipment.isOccupied(EquipmentSlot.OFFHAND);
-    }
-
-    public static void equipGear(){
-        int attempt = 0;
-        while (!gearEquipped() && attempt < 3) {
-            Item[] items = Inventory.getItems(x -> x.containsAction("Wield") || x.containsAction("Wear"));
-            for (Item i : items) {
-                i.click();
-                Time.sleep(50, 300);
+        Player local = Players.getLocal();
+        Target target = Main.getCurrentTarget();
+        if (target.getZone().contains(local)) {
+            if (local.isMoving())
+                Time.sleepUntil(local::isMoving, 2000);
+            else if (local.getTargetIndex() != -1)
+                Time.sleepUntil(() -> local.getTargetIndex() == -1, 5000);
+            else {
+                fightNPC(x -> target.matches(x)
+                        && ((x.getTarget() != null && x.getTarget().equals(local)) || x.getTargetIndex() == -1)
+                        && x.getHealthPercent() > 0
+                        && Movement.isInteractable(x.getPosition()));
             }
-            Time.sleepUntil(Inventory::isEmpty, 2000);
-            attempt++;
-        }
-        if (!gearEquipped()){
-            Log.info("Attempted to equip gear 3 times but failed.");
-            Main.updateScriptState(null);
-        }
+        } else
+            Main.updateScriptState(ScriptState.WALKING);
+
     }
+
+    //Fights an npc that matches the given predicate
+    private static void fightNPC(Predicate<Npc> targetPredicate) {
+        Npc npc = Npcs.getNearest(targetPredicate);
+        if (npc != null && npc.interact("Attack"))
+            Time.sleep(200, 600);
+
+    }
+
 
     /**
      * Calculates the lowest combat skill and returns the attack style that will train it.
      * @return Returns the most optimal attack style to train.
      */
-    public static Combat.AttackStyle getBestAttackStyle(){
+    public static Combat.AttackStyle getBestAttackStyle() {
         int attackLevel = Skills.getLevel(Skill.ATTACK);
         int strengthLevel = Skills.getLevel(Skill.STRENGTH);
         int defenceLevel = Skills.getLevel(Skill.DEFENCE);
 
-            int lowestCMBLevel = strengthLevel;
-            Combat.AttackStyle bestAttackStyle = Combat.AttackStyle.AGGRESSIVE;
+        int lowestCMBLevel = strengthLevel;
+        Combat.AttackStyle bestAttackStyle = Combat.AttackStyle.AGGRESSIVE;
 
-            if (attackLevel < lowestCMBLevel) {
-                lowestCMBLevel = attackLevel;
-                bestAttackStyle = Combat.AttackStyle.ACCURATE;
-            }
-            if (defenceLevel < lowestCMBLevel) {
-                bestAttackStyle = Combat.AttackStyle.DEFENSIVE;
-            }
+        if (attackLevel < lowestCMBLevel) {
+            lowestCMBLevel = attackLevel;
+            bestAttackStyle = Combat.AttackStyle.ACCURATE;
+        }
+        if (defenceLevel < lowestCMBLevel) {
+            bestAttackStyle = Combat.AttackStyle.DEFENSIVE;
+        }
 
         return bestAttackStyle;
     }
