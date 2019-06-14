58aee97a30985ebbda355117780caf57ba187777
James - Desktop <willcocksj@hotmail.co.uk>
2019-06-15 00:14:12 +0100

    Added seeds to keep

--- src/Master_Thiever/Main.java
+++ src/Master_Thiever/Main.java
@@ -49,7 +49,7 @@ public class Main extends Script implements ChatMessageListener {
         return foodName;
     }
 
-    private static final String[] seedsToKeep = {"Snape grass", "Ranarr", "Snapdragon", "Torstol", "Avantoe", "Lantadyme"};
+    private static final String[] seedsToKeep = {"Snape grass", "Ranarr", "Snapdragon", "Torstol", "Avantoe", "Lantadyme", "Wildblood", "Yanillian", "Strawberry"};
     public static String[] getSeedsToKeep(){
         return seedsToKeep;
     }
