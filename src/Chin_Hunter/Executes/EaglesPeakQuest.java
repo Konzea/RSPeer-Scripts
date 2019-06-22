d1a2c2f693e91e9f9c7a69442c09ef05d7f30d97
James - Desktop <willcocksj@hotmail.co.uk>
2019-06-22 01:12:50 +0100

    Updated naming conventions and split required items to required items and minimum required items

--- src/Chin_Hunter/Executes/EaglesPeakQuest.java
+++ src/Chin_Hunter/Executes/EaglesPeakQuest.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public class EaglesPeakQuest {
 
-    private static final Map<String, Integer> requiredItems = new HashMap<>();
+    private static final Map<String, Integer> REQUIRED_ITEMS = new HashMap<>();
 
     private EaglesPeakQuest(){
         //Private default constructor
@@ -18,23 +18,28 @@ public class EaglesPeakQuest {
 
     }
 
+    public static Map<String, Integer> getRequiredItems(){
+        return REQUIRED_ITEMS;
+    }
+
+
     public static void populateHashMap(){
-        if (requiredItems.isEmpty()) {
-            requiredItems.put("Yellow dye", 1);
-            requiredItems.put("Swamp tar", 1);
-            requiredItems.put("Coins", 50);
-            requiredItems.put("Necklace of passage", 1);
-            requiredItems.put("Camelot teleport", 5);
+        if (REQUIRED_ITEMS.isEmpty()) {
+            REQUIRED_ITEMS.put("Yellow dye", 1);
+            REQUIRED_ITEMS.put("Swamp tar", 1);
+            REQUIRED_ITEMS.put("Coins", 50);
+            REQUIRED_ITEMS.put("Necklace of passage(5)", 1);
+            REQUIRED_ITEMS.put("Camelot teleport", 5);
         }
     }
 
     public static boolean hasAllRequiredItems(){
-        if (requiredItems.isEmpty()) {
+        if (REQUIRED_ITEMS.isEmpty()) {
             Log.severe("Hashmap not populated.");
             Main.updateScriptState(null);
             return false;
         }
-        return Main.hasItems(requiredItems);
+        return Main.hasItems(REQUIRED_ITEMS);
     }
 
 
