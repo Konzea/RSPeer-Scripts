c0bddcd623cdcc380d8ffbdc6453e94cbb46591e
James - Desktop <willcocksj@hotmail.co.uk>
2019-08-06 22:59:05 +0100

    initial empty commit

--- src/Chin_Hunter/Executes/Herblore/Herblore_Training.java
+++ src/Chin_Hunter/Executes/Herblore/Herblore_Training.java
@@ -1,9 +1,13 @@
 package Chin_Hunter.Executes.Herblore;
 
+import Chin_Hunter.Main;
+import org.rspeer.ui.Log;
+
 public class Herblore_Training {
 
     public static void execute() {
-
+        Log.info("Currently herblore training is not supported.");
+        Main.updateScriptState(null);
     }
 
     public static void onStart() {
